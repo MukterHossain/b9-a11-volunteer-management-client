52094b4c7a0e8757a09a25dc4e764b54dae279c8
MukterHossain <mukterhossain3075@gmail.com>
2024-05-10 09:37:49 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Heaater</h1>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,20 @@
+
+import {createBrowserRouter,} from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+        {
+            
+        }
+    ]
+  },
+]);
+
+
+export default router;
+
