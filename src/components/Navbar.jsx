d1eeb8548d888778c3ac28fdf3a25b9531e67523
MukterHossain <mukterhossain3075@gmail.com>
2024-05-14 16:25:41 +0600

    added light and dark mode and some fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,37 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 
 
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext)
+    const [theme, setTheme] = useState('light');
     const navLinks = <>
 
-        < NavLink to='/' className={({ isActive }) => isActive ? 'py-1 px-3 border-2  rounded-lg  font-bold text-green-400 border-green-400' : 'font-bold py-1 px-3 rounded-lg border-2 border-gray-200 '}>Home</NavLink>
+        < NavLink to='/' className={({ isActive }) => isActive ? 'py-1 px-3 border-2  rounded-lg   font-bold text-green-400 border-green-400' : 'font-bold py-1 px-3 rounded-lg border-2 border-gray-200 '}>Home</NavLink>
         <NavLink to='/need-volunteer' className={({ isActive }) => isActive ? 'py-1 px-3 border-2  rounded-lg  font-bold text-green-400 border-green-400' : 'font-bold py-1 px-3 rounded-lg border-2 border-gray-200 '}>Need Volunteer</NavLink>
         <NavLink to='/volunteer-details' className={({ isActive }) => isActive ? 'py-1 px-3 border-2  rounded-lg  font-bold text-green-400 border-green-400' : 'font-bold py-1 px-3 rounded-lg border-2 border-gray-200 '}>Volunteer Need Post Details</NavLink>
-        {/* {
-            user && <>
-                <NavLink to='/' className={({ isActive }) => isActive ? 'py-1 px-3 rounded-sm  font-bold bg-green-400 text-white' : 'font-bold py-1 px-3 rounded-sm border-2 border-gray-200 '}>Add Tourists Spot</NavLink>
-            </>
-        } */}
 
     </>
+
+    useEffect(() => {
+        localStorage.setItem('theme', theme)
+        const localTheme = localStorage.getItem('theme')
+        document.querySelector('html').setAttribute('data-theme', localTheme)
+    }, [theme])
+    const handleToggle = (e) => {
+        if (e.target.checked) {
+            setTheme('synthwave')
+        }
+        else {
+            setTheme('light')
+        }
+    }
+
+
+
     return (
-        <div> 
+        <div>
 
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
@@ -53,30 +66,37 @@ const Navbar = () => {
                         >
                             My Profile
                         </div>
-                        
+
                         <ul
                             tabIndex={0}
                             className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 space-y-2'
                         >
 
                             <li>
-                            < NavLink to='/add-volunteer' className={({ isActive }) => isActive ? 'py-1 px-3 border-2  rounded-xl  font-bold text-green-400 border-green-400' : 'font-bold py-1 px-3 rounded-xl border-2 border-gray-200 '}>Add Volunteer Post</NavLink>
+                                < NavLink to='/add-volunteer' className={({ isActive }) => isActive ? 'py-1 px-3 border-2  rounded-xl  font-bold text-green-400 border-green-400' : 'font-bold py-1 px-3 rounded-xl border-2 border-gray-200 '}>Add Volunteer Post</NavLink>
                                 {/* <Link to={'/add-volunteer'} className='justify-between'> Add Volunteer Post</Link> */}
                             </li>
                             <li>
-                            < NavLink to='/manage-my-post' className={({ isActive }) => isActive ? 'py-1 px-3 border-2  rounded-xl  font-bold text-green-400 border-green-400' : 'font-bold py-1 px-3 rounded-xl border-2 border-gray-200 '}>Manage My Post</NavLink>
+                                < NavLink to='/manage-my-post' className={({ isActive }) => isActive ? 'py-1 px-3 border-2  rounded-xl  font-bold text-green-400 border-green-400' : 'font-bold py-1 px-3 rounded-xl border-2 border-gray-200 '}>Manage My Post</NavLink>
                                 {/* <Link to={'/manage-my-post'}> Manage My Post</Link> */}
                             </li>
                         </ul>
+                    </div>
+                    <div>
+                        <label className="cursor-pointer grid place-items-center">
+                            <input onChange={handleToggle} type="checkbox" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
+                            <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
+                            <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                        </label>
                     </div>
 
                     {
                         user ?
                             <div className="dropdown dropdown-end">
                                 <div className="flex justify-around items-center">
-                                    <label  tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                    <label tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                                         <div title={user?.displayName || 'user name not found'} className="w-10 rounded-full">
-                                            <img referrerPolicy="no-referrer"  src={user?.photoURL || "https://i.ibb.co/pnNbsKQ/sundarban.jpg"} />
+                                            <img referrerPolicy="no-referrer" src={user?.photoURL || "https://i.ibb.co/pnNbsKQ/sundarban.jpg"} />
                                         </div>
                                     </label>
                                     {/* onClick={handleLogOut} */}
